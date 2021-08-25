b53044884e6be3868a33acd06724525b2507e13a
rina-333 <rina.career.s@gmail.com>
2021-08-25 15:13:50 +0700

    edit config, package and package.json for deploy server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require( 'express' )
 const app = express()
 
-const port = 3333
+const port = process.env.PORT || 3333
 const router = require( './routes' ) //index.js auto baca
 
 app.set( 'view engine', 'ejs' )
